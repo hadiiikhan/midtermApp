fc17b4588228b84cf896be72657f174ba6f3bbfe
Hadi Khan <hadimikasa1@gmail.com>
2024-10-15 14:55:11 -0400

    updated top bar

---
+++ app/src/main/java/ca/khanhadi/midtermapp/ui/theme/ProductTopAppBar.kt
@@ -0,0 +1,35 @@
+package ca.khanhadi.midtermapp.ui.theme
+
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.TopAppBarScrollBehavior
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import ca.khanhadi.midtermapp.R
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ProductTopAppBar(
+    title: String,
+    modifier: Modifier = Modifier,
+    scrollBehavior: TopAppBarScrollBehavior? = null,
+    navigateUp: () -> Unit = {}
+) {
+    CenterAlignedTopAppBar(
+        title = { Text(title) },
+        modifier = modifier,
+        scrollBehavior = scrollBehavior,
+        colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
+            containerColor = MaterialTheme.colorScheme.primaryContainer
+        )
+    )
+}
