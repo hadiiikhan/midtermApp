80339919de4dbb1f35b22f2c164404f437bda9c1
Hadi Khan <hadimikasa1@gmail.com>
2024-10-15 14:14:54 -0400

    Initial commit

---
+++ app/src/main/java/ca/khanhadi/midtermapp/ui/theme/Product.kt
@@ -0,0 +1,10 @@
+package ca.khanhadi.midtermapp.ui.theme
+
+data class Product(
+    val name: String,
+    val type: String,
+    val colour: String,
+    val genderStyle: String,
+    val price: Double
+)
+
